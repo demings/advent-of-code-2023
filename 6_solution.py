a35d6d9a0e791b9035ce77958d2b5af5449ec843
Mindaugas Laskis <Mindaugas.Laskis@bentley.com>
2023-12-06 11:15:45 +0200

    Implement day 6 part 2

--- 6_solution.py
+++ 6_solution.py
@@ -1,16 +1,12 @@
 with open("6_input.txt") as f:
     lines = f.readlines()
 
-times = list(map(lambda x: int(x), lines[0].split(":")[1].strip().split())) 
-distances = list(map(lambda x: int(x), lines[1].split(":")[1].strip().split()))
+time = int(lines[0].split(":")[1].replace(" ", ""))
+distance = int(lines[1].split(":")[1].replace(" ", ""))
 
-multiples = 1
-for idx, time in enumerate(times):
-    nums_to_record = 0
-    for x in range(time):
-        if (x * (time - x)) > distances[idx]:
-            nums_to_record += 1
-    if nums_to_record > 0:
-        multiples *= nums_to_record
+nums_to_record = 0
+for x in range(time):
+    if (x * (time - x)) > distance:
+        nums_to_record += 1
 
-print(multiples)
+print(nums_to_record)
