630814a35f3cfbf3b1a76cbada9b3d07a5b67d06
Mindaugas Laskis <Mindaugas.Laskis@bentley.com>
2023-12-08 11:36:08 +0200

    Implement day 8 part 2

--- 8_solution.py
+++ 8_solution.py
@@ -1,3 +1,5 @@
+import math
+
 with open("8_input.txt") as f:
     lines = f.readlines()
 
@@ -9,18 +11,22 @@ for line in lines[2:]:
     [key, value] = line.replace("\n", "").split(" = ")
     network[key] = value[1:-1].split(", ")
 
-count = 0
-current_key = "AAA"
-current_instruction_idx = 0
-while current_key != "ZZZ":
-    count += 1
+counts = []
+current_keys = [x for x in network.keys() if x.endswith("A")]
+for current_key in current_keys:
+    count = 0
+    current_instruction_idx = 0
+    while not current_key.endswith("Z"):
+        count += 1
 
-    current_key = network[current_key][
-        0 if instructions[current_instruction_idx] == "L" else 1
-    ]
+        current_key = network[current_key][
+            0 if instructions[current_instruction_idx] == "L" else 1
+        ]
 
-    current_instruction_idx += 1
-    if current_instruction_idx >= len(instructions):
-        current_instruction_idx = 0
+        current_instruction_idx += 1
+        if current_instruction_idx >= len(instructions):
+            current_instruction_idx = 0
 
-print(count)
+    counts.append(count)
+
+print(math.lcm(*counts))
